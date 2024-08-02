9646c335fac847e270e9035a2458b6c48f076d2c
Ryan Broad <ryanb@punterstech.com>
2024-08-02 13:59:52 +1000

    remove prefix

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 from app.api.endpoints import platemod
 
-
 api_router = APIRouter()
-api_router.include_router(platemod.router, prefix="/platemod/v1", tags=["platemod"])
+api_router.include_router(platemod.router, prefix="/v1", tags=["platemod"])
