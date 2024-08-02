723ea240332f0ff25951135d443f22e1d4e3f1a8
Ryan Broad <ryanb@punterstech.com>
2024-08-02 14:32:42 +1000

    add CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import api_router
 
 app = FastAPI()
+
+# Set up CORS
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:5173",
+    "http://127.0.0.1",
+    "http://127.0.0.1:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
